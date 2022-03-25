7c2fce6b7abcffa5a12010fd25fa72aea5221175
ViktoriaUlko <ulko@firecode.ru>
2022-03-25 11:12:12 +0300

    Песенка про пиво

--- BeerSong/BeerSong.java
+++ BeerSong/BeerSong.java
@@ -1,9 +1,9 @@
 
 public class BeerSong {
-	public static void main (String[] args) {
+	public static void main(String[] args) {
 		int beerNum = 99;
 		String word = "бутылок (бутылки)";
-		while (beerNum >= 1){
+		while (beerNum >= 1) {
 			if (beerNum == 1) {
 				word = "бутылка  ";
 			}
@@ -12,10 +12,9 @@ public class BeerSong {
 			System.out.println("Возьми одну.");
 			System.out.println("Пусти по кругу.");
 			beerNum = beerNum - 1;
-			if (beerNum < 0){
-			System.out.println(beerNum  +  " " +  word  +  "пива на стене");	
-			}
-			else if(beerNum == 0){
+			if (beerNum < 0) {
+				System.out.println(beerNum + " " + word + "пива на стене");
+			} else if (beerNum == 0) {
 				System.out.println("Нет бутылок пива на стене");
 			}
 		}

ab9b927cdf87624388feb94239f31c33f10bbe5e
ViktoriaUlko <ulko@firecode.ru>
2022-04-06 22:44:24 +0300

    Версия c Андрюшей

--- Game/src/Game.java
+++ Game/src/Game.java
@@ -1,22 +1,37 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Game {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        int x = (int) (Math.random() * 10);
-        int y;
-        int z = 0;
+        int numberPC = new Random().nextInt(10);
+        int numberUser;
+        int triesLimit = 5;
+        int tries = 0;
+        boolean isWin = true;
+//        чтобы знать что загадал комп
+//        System.out.println(numberPC);
+        System.out.println("Введите число от 0 до 10");
         do {
-            System.out.println("Введите число от 0 до 10");
-            y = s.nextInt();
-            if (x > y) {z = z + 1;
-                System.out.println("Подумай еще! Число должно быть больше. Попытка № " + z);
+            numberUser = s.nextInt();
+            tries++;
+            if (tries == triesLimit) {
+                isWin = false;
+                break;
             }
-            if (x < y) {z = z + 1;
-                System.out.println("Попробуй еще раз, число должно быть меньше. Попытка № " + z);
+            if (numberPC > numberUser) {
+                System.out.println("Подумай еще! Число должно быть больше. Количество оставшихся попыток "
+                        + (triesLimit - tries));
             }
-        } while (x != y) ;
-        z = z + 1;
-            System.out.println("С попытки № " + z + " ты угадал ");
+            if (numberPC < numberUser) {
+                System.out.println("Попробуй еще раз, число должно быть меньше. Количество оставшихся попыток "
+                        + (triesLimit - tries));
+            }
+        } while (numberPC != numberUser);
+        if (isWin) {
+            System.out.println("С попытки № " + tries + " ты угадал ");
+        } else {
+            System.out.println("Попытки закончились))) Ты - тормоз!");
+        }
     }
 }
